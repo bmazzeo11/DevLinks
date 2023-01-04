c3dd9071c41f06a94087f6c949ff99b2eafdfe5d
Bruno Marcel <bmazzeo11@outlook.com>
2023-01-03 21:55:00 -0300

    changed script.js

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
-function toggleMod () {
+function toggleMod() {
   const html = document.documentElement
-   html.classList.toggle("light")
- 
+  html.classList.toggle("light")
+
   //pegar a img
-    const img = document.querySelector("#profile img")
+  const img = document.querySelector("#profile img")
 
-    //substituir a imagem
-   if(html.classList.contains('light')) {
+  //substituir a imagem
+  if (html.classList.contains("light")) {
     //se tiver light mode, adicionar a imagem
-    img.setAttribute('src', './assets/avatar-light2.png')
-
-   } else {
+    img.setAttribute("src", "./Assets/avatar-light.jpg")
+  } else {
     //se tiver sem light mode, manter a imagem normal
-    img.setAttribute('src', './assets/avatar.png')
-   }
-
-
-   }
- 
+    img.setAttribute("src", "./Assets/avatar.png")
+  }
+}
